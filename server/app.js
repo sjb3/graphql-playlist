230f899c212983542d0eac4c9b4c870b87a85079
Sung J Byun <sungjbyun@D-128-95-134-165.dhcp4.washington.edu>
2018-04-10 18:56:20 -0700

    now adding front end and sync with the backend server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+const app = express();
+// allow cross-origin-request
+app.use(cors());
 
 // connect to mlab db
 mongoose.connect('mongodb://sjb3:password@ds035643.mlab.com:35643/graphql-playlist');
@@ -12,7 +18,7 @@ mongoose.connection.once('open', () => {
 
 const schema = require('./schema/schema');
 
-const app = express();
+
 const PORT = process.env.PORT || 4000;
 
 // bind express with graphql
