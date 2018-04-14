990ef29a2a6ce1a08ef8c3ba568b7bab3ebfe52f
Sung J Byun <sungjbyun@D-173-250-158-242.dhcp4.washington.edu>
2018-04-13 21:11:37 -0700

    working on front-end: find option tag does not work with materialcss?

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -12,13 +12,26 @@ const getBooksQuery = gql`
 `;
 
 class BookList extends Component {
+  displayBooks() {
+    var data = this.props.data;
+    if(data.loading) {
+      return (
+      <div>Loading Books...</div>
+      )} else {
+      return data.books.map((book) => (
+        <li key={book.id}>{book.name}</li>
+      ))
+    }
+  }
+
   render() {
-    console.log(this.props);
+    // console.log(this.props);
 
     return (
       <div>
         <h3>Book List</h3>
           <ul id='book-list'>
+          {this.displayBooks}
           </ul>
       </div>
     )
